ec9f00478143995c09cbc6a3d27e1314c4ff0dac
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-10-20 17:16:57 +0300

    Add more libs, fetch stock data, plot graph

---
+++ main.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+from matplotlib import pyplot as plt
+
+google = yf.Ticker("GOOG")
+
+df = google.history(period="1d", interval="1m")[["Low"]]
+df["date"] = pd.to_datetime(df.index).time
+df.set_index("date", inplace=True)
+
+X = df.index.values
+y = df["Low"].values
+
+offset = int(0.10 * len(df))
+
+X_train = X[:-offset]
+y_train = y[:-offset]
+X_test = X[-offset:]
+y_test = y[-offset:]
+
+plt.plot(range(0, len(y_train)), y_train, label="Train")
+plt.plot(range(len(y_train), len(y)), y_test, label="Test")
+plt.legend()
+plt.show()
